7b741dc1a17ffe735d11e23f115407e98c1eaccc
DarianTr <darian@tratar.de>
2026-03-04 10:49:33 +0100

    working on auth, added middleware

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -11,33 +11,35 @@ func SetupRoutes(r *gin.Engine, dbCon *pg.DB, cfg *config.Config) {
 	h := NewHandler(dbCon, cfg)
 	authHandler := auth.NewAuthHandler(dbCon)
 
+	protected := r.Group("/", authHandler.AuthMiddleware())
+
 	// Resources
-	r.GET("/organisations", h.GetOrganisations)
-	r.GET("/organisations/:orgId/buildings", h.GetBuildings)
-	r.GET("/organisations/:orgId/rooms", h.GetRooms)
-	r.GET("/organisations/:orgId/shelves", h.GetShelves)
-	r.GET("/organisations/:orgId/inventory", h.GetInventory) // ?start=X&end=X
-	r.POST("/organisations/:orgId/buildings", h.CreateBuilding)
-	r.POST("/organisations/:orgId/buildings/:buildingId/rooms", h.CreateRoom)
-	r.POST("/organisations/:orgId/buildings/:buildingId/rooms/:roomId/shelves", h.CreateShelf)
+	protected.GET("/organisations", h.GetOrganisations)
+	protected.GET("/organisations/:orgId/buildings", h.GetBuildings)
+	protected.GET("/organisations/:orgId/rooms", h.GetRooms)
+	protected.GET("/organisations/:orgId/shelves", h.GetShelves)
+	protected.GET("/organisations/:orgId/inventory", h.GetInventory) // ?start=X&end=X
+	protected.POST("/organisations/:orgId/buildings", h.CreateBuilding)
+	protected.POST("/organisations/:orgId/buildings/:buildingId/rooms", h.CreateRoom)
+	protected.POST("/organisations/:orgId/buildings/:buildingId/rooms/:roomId/shelves", h.CreateShelf)
 
 	// Items
-	r.GET("/organisations/:orgId/items/:id", h.GetItem) // ?start=X&end=X
-	r.POST("/organisations/:orgId/items", h.CreateItem)
-	r.PUT("/organisations/:orgId/items/:id", h.UpdateItem)
-	r.GET("/organisations/:orgId/items/:id/borrows", h.GetBorrowHistory)
+	protected.GET("/organisations/:orgId/items/:id", h.GetItem) // ?start=X&end=X
+	protected.POST("/organisations/:orgId/items", h.CreateItem)
+	protected.PUT("/organisations/:orgId/items/:id", h.UpdateItem)
+	protected.GET("/organisations/:orgId/items/:id/borrows", h.GetBorrowHistory)
 
 	// Cart
-	r.GET("/users/:userId/cart", h.GetShoppingCart) // ?start=X&end=X
-	r.POST("/users/:userId/cart/items", h.CreateCartItem)
-	r.POST("/users/:userId/cart/checkout", h.CheckoutCart)
+	protected.GET("/users/:userId/cart", h.GetShoppingCart) // ?start=X&end=X
+	protected.POST("/users/:userId/cart/items", h.CreateCartItem)
+	protected.POST("/users/:userId/cart/checkout", h.CheckoutCart)
 
 	// Loans & Requests
-	r.PUT("/loans/:id", h.UpdateLoan)
-	r.PUT("/requests/:id", h.UpdateRequest)
-	r.POST("/requests/:id/review", h.RequestReview)
-	r.GET("/requests/:id/messages", h.GetMessages)
-	r.POST("/requests/:id/messages", h.PostMessage)
+	protected.PUT("/loans/:id", h.UpdateLoan)
+	protected.PUT("/requests/:id", h.UpdateRequest)
+	protected.POST("/requests/:id/review", h.RequestReview)
+	protected.GET("/requests/:id/messages", h.GetMessages)
+	protected.POST("/requests/:id/messages", h.PostMessage)
 
 	// Auth
 	r.GET("/auth/eduid/login", authHandler.LoginHandler)
